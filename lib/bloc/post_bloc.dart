1540f398d28817a87c162f11179e7021aaf49d44
Armando Ruiz <aranruiz13@gmail.com>
2021-02-12 12:01:20 -0600

    Add logic to retrieve post from network

--- lib/bloc/post_bloc.dart
+++ lib/bloc/post_bloc.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_infinite_list_app/bloc/bloc.dart';
+import 'package:flutter_infinite_list_app/models/post.dart';
 import 'package:http/http.dart' as http;
 
 class PostBloc extends Bloc<PostEvent, PostState> {
@@ -10,6 +13,39 @@ class PostBloc extends Bloc<PostEvent, PostState> {
 
   @override
   Stream<PostState> mapEventToState(PostEvent event) async* {
-    yield null;
+    final currentState = state;
+    if (event is PostFetched && !_hasReachedMax(currentState)) {
+      try {
+        if (currentState is PostInitial) {
+          final posts = await _fetchPosts(0, 20);
+          yield PostSuccess(posts, false);
+          return;
+        }
+        if (currentState is PostSuccess) {
+          final posts = await _fetchPosts(currentState.posts.length, 20);
+          yield posts.isEmpty
+              ? currentState.copyWith(hasReachedMax: true)
+              : PostSuccess(currentState.posts + posts, false);
+        }
+      } catch (_) {
+        yield PostFailure();
+      }
+    }
+  }
+
+  bool _hasReachedMax(PostState state) =>
+      state is PostSuccess && state.hasReachedMax;
+
+  Future<List<Post>> _fetchPosts(int startIndex, int limit) async {
+    final response = await httpClient.get(
+        'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit');
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List;
+      return data
+          .map((rawPost) =>
+              Post(rawPost['id'], rawPost['title'], rawPost['body']))
+          .toList();
+    } else
+      throw Exception('error fetching posts');
   }
 }
